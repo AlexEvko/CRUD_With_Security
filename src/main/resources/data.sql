710a89f56683b344ba59a67a989e53ce581dd261
AlexEvk <rec>
2022-07-12 22:42:05 +0300

    work

--- src/main/resources/data.sql
+++ src/main/resources/data.sql
@@ -12,3 +12,7 @@ USE my_db;
 
 INSERT INTO users values(5,'Vlad', 'Nedob', 22, 'Nedob'), (6,'Vla', 'N', 22, 'N');
 INSERT INTO users_roles values(1, 5), (1, 6);
+
+INSERT INTO role values(1,'ROLE_ADMIN'), (2,'ROLE_USER');
+INSERT INTO users values(5,'Vlad', 'Nedob', 22, 'Nedob'), (6,'Vla', 'N', 22, 'N');
+INSERT INTO users_roles values(1, 5), (1, 6);
